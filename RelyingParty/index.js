6b59e28d30a5213494bd1cef3f90a8fb7472a49a
AlfredSchou <abjarneg@live.dk>
2025-04-30 12:32:28 +0200

    added post endpoint for RP to recieve person id in json format

--- RelyingParty/index.js
+++ RelyingParty/index.js
@@ -2,14 +2,17 @@ import 'dotenv/config';
 import express from 'express';
 import pingRouter    from './src/routes/ping.route.js';
 import sessionRouter from './src/routes/session.route.js';
+import purchaseRouter from './src/routes/purchase.route.js';
 
 const app = express();
 app.use(express.json());
 
 app.use('/ping',    pingRouter);
 app.use('/session', sessionRouter);
+app.use('/purchase', purchaseRouter);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log(`RelyingParty service is running http://localhost:${PORT}`);	
+    
 })
